bea78c5ac03d6df7130cc4dacb198ea054d23947
Mimi Cong <mina123fire@yahoo.com>
2020-08-25 17:27:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var kittens = ["Milo","Otis","Garfield"] //define your array here
 
 function destructivelyAppendKitten(name) {
-  returns kittens.push(name);
+   kittens.push(name);
 }
 // Add your functions and code here
